9b8c9ee570733aa2af06e3d102b211e7bad41bf7
TG-huaer11 <huaer0557@gmail.com>
2026-09-04 11:47:51 +0800

    Add files via upload

---
+++ tiktok/config/site.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    "name" => "鼎盛集团",
+    "beian" => "",
+    "cdnurl" => "",
+    "version" => "1.0.12",
+    "timezone" => "Asia/Shanghai",
+    "forbiddenip" => "",
+    "languages" => [
+        "backend" => "zh-cn",
+        "frontend" => "Arabic"
+    ],
+    "fixedpage" => "dashboard",
+    "categorytype" => [
+        "default" => "默认",
+        "page" => "单页",
+        "article" => "文章",
+        "test" => "Test"
+    ],
+    "configgroup" => [
+        "basic" => "基础配置",
+        "email" => "邮件配置",
+        "dictionary" => "字典配置",
+        "user" => "会员配置",
+        "regsiter" => "注册分组"
+    ],
+    "mail_type" => "1",
+    "mail_smtp_host" => "smtp.qq.com",
+    "mail_smtp_port" => "465",
+    "mail_smtp_user" => "10000",
+    "mail_smtp_pass" => "password",
+    "mail_verify_type" => "2",
+    "mail_from" => "10000@qq.com",
+    "attachmentcategory" => [
+        "category1" => "分类一",
+        "category2" => "分类二",
+        "custom" => "自定义"
+    ],
+    "default_country_code" => "91",
+    "orange_id" => "207-India",
+    "orange_type" => "yd",
+    "login_background_image" => "/assets/default/img/login-bg.png",
+    "login_logo" => "/assets/default/img/logo.png",
+    "site_color" => "#e89402",
+    "order_model" => "1",
+    "order_effective_time" => "100000",
+    "frontend_template" => "prefeshop",
+    "give_amount" => "0",
+    "day_order_num" => "60",
+    "kefu" => "",
+    "withdraw_limit" => "0",
+    "share_url" => "/h5/#/pages/login/register"
+];
