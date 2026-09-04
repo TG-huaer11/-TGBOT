f4728ade83a12fba3c965c5d1f9371768e707b28
TG-huaer11 <huaer0557@gmail.com>
2026-09-04 15:51:03 +0800

    Delete tiktok directory

--- tiktok/app/admin/lang/zh-cn/dashboard.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'Register'                 => '注册',
-    'Recharge'                 => '充值',
-    'Withdraw'                 => '取款',
-    'Profit'                   => '盈利'
-];
--- tiktok/runtime/cache/bf/9ffa4f77aefd409e46c423f9a75d38.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:1:{i:0;s:76:"/www/wwwroot/tiktokv.fit/runtime/cache/cb/78ef4a9130f39208ff10858ddd8377.php";}
